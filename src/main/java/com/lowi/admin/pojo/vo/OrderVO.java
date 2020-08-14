6f51d79afd62126530cbca56953ba8e69735c268
gengyy <gengyy@51lick.cn>
2020-08-14 17:22:41 +0800

    订单处理

---
+++ src/main/java/com/lowi/admin/pojo/vo/OrderVO.java
@@ -0,0 +1,17 @@
+package com.lowi.admin.pojo.vo;
+
+/**
+ * OrderVO.java
+ * ==============================================
+ * Copy right 2015-2017 by http://www.51lick.com
+ * ----------------------------------------------
+ * This is not a free software, without any authorization is not allowed to use and spread.
+ * ==============================================
+ *
+ * @author : gengyy
+ * @version : v2.0
+ * @desc :
+ * @since : 2020/8/14 16:19
+ */
+public class OrderVO {
+}
